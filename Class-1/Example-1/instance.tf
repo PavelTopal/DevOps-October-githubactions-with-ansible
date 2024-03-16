a1d045a7518efc5cbd33e5866208e3c0cfdbd551
PavelTopal <paveltopal0@gmail.com>
2024-03-16 01:07:45 -0500

    Deleted all the files

--- Class-1/Example-1/instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "myinstance" {
-  ami           = "ami-079db87dc4c10ac91"
-  instance_type = "t2.micro"
-  key_name      = "admin-key"
-
-  tags = {
-    Name = "myinstance"
-  }
-}
